b18ca906dd30c0d8f053ccd5ed0435d681d0da02
mostak9 <mostak.ahm263@gmail.com>
2023-12-11 18:17:16 +0600

    added skils section

--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
-import Lottie from "lottie-react";
+
 import Title from "../../Shared/Title/Title";
-import aboutAnimation from '../../../assets/about.json'
+
 import roundProfile from '../../../assets/round_profile.png';
 import { MdEmail } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
@@ -9,16 +9,17 @@ import { FaLocationDot } from "react-icons/fa6";
 
 const About = () => {
   return (
-    <div id="about" className="max-w-5xl mx-auto py-12 px-5 lg:px-0">
+    <div className="">
+        <div id="about" className="max-w-5xl mx-auto py-12 px-5 lg:px-0">
       <Title text="About Me" />
-      <div className="flex flex-col-reverse md:flex-row items-center  gap-2">
+      <div className="flex flex-col-reverse md:flex-row items-center  gap-5">
         <div className="">
-          <p className="max-w-md">
+          <div className="max-w-md">
             <h1 className="text-2xl font-medium">Hey, I'm <span className="text-green-500">Mostak Ahmed</span>,</h1>
             <br/> A budding developer with a knack for
             Front-end, React, and MERN stack technologies.<br/> 
             <p>Currently on the journey to earn my B.Sc. in Electrical and Electronic Engineering (EEE) at Hajee Mohammad Danesh Science and Technology University</p>
-          </p>
+          </div>
           <div className="mt-3 space-y-2">
             <p className=" flex items-center gap-1"><MdEmail className="text-green-500 text-2xl"/> mostak.ahm263@gmail.com</p>
             <p className=" flex items-center gap-1"><FaPhoneAlt className="text-green-500 text-2xl"/> +880 1705833263</p>
@@ -30,6 +31,7 @@ const About = () => {
             <img src={roundProfile} className="w-1/2 mx-auto rounded-full shadow-xl shadow-green-500" alt="" />
         </div>
       </div>
+    </div>
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../../Components/Home/About/About";
 import Banner from "../../Components/Home/Banner/Banner";
 import Projects from "../../Components/Home/Projects/Projects";
+import Skills from "../../Components/Home/Skills/Skills";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div className="lg:max-w-[calc(100vw-22rem) lg:w-[calc(100vw-21rem)] lg:ml-auto" >
            <Banner/> 
            <About/>
+           <Skills/>
            <Projects/>
         </div>
     );
